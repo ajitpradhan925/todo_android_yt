6d7d0612f772f50d2856e96b3c93d060b0b3e4ab
Ajit Pradhan <codewithajit@gmail.com>
2020-07-29 10:36:22 -0700

    new

---
+++ app/src/main/java/com/codewithajit/todoapp/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.codewithajit.todoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                    startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                    finish();
+                }catch (Exception e) {
+
+                }
+            }
+        };
+
+        thread.start();
+    }
+}
